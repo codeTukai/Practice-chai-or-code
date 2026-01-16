ea4dd07538cbb9c96fc25f5c75915cbcf2ace98b
codeTukai <tukaighosh753@gmail.com>
2026-01-16 20:48:52 +0530

    change

--- Array_Function.js
+++ Array_Function.js
@@ -1,7 +1,7 @@
 const marvel_heros = ["thor", "Ironman", "Spiderman"]
 const dc_heros = ["Superman", "flash", "Batman"]
 
-// marvel_heros.push(dc_heros) //work on presenting/existing array 
+marvel_heros.push(dc_heros) //work on presenting/existing array 
 
 // console.log(marvel_heros);
 // console.log(marvel_heros.length);
@@ -11,20 +11,20 @@ const dc_heros = ["Superman", "flash", "Batman"]
 // const a = marvel_heros.concat(dc_heros) //its represented a new array
 // console.log(a);
 
-const a1 =[...marvel_heros, ...dc_heros]
-console.log(a1);
+const a1 =[...marvel_heros, ...dc_heros]   //spread operator  ---- add multiple arrays 
+// console.log(a1);
 
 const another_Array = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4,5]]]
 
 const real_another_Array = another_Array.flat(Infinity)
 console.log(real_another_Array);
 
-console.log(Array.isArray("Tukai"));
-console.log(Array.from("Tukai"));
+console.log(Array.isArray("Tukai")); //asked 
+console.log(Array.from("Tukai")); //convert into array
 
-console.log(Array.from({name:"tukai"})); //return an empty object when its define as key value pairs ---[]
+console.log(Array.from({name:"tukai"})); //**** return an empty object when its define as a key value pairs ---[]
 
-let score1 = 100
+let score1 = "tukai"
 let score2 = 200
 let score3 = 300
 
