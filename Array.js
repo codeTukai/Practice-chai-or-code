3c7409cd210c82caabeae6e9a3c478ba6e73bb57
codeTukai <tukaighosh753@gmail.com>
2026-01-16 19:33:18 +0530

    change

--- Array.js
+++ Array.js
@@ -1,42 +1,69 @@
 const myArr = [10, 20, 22, 30 ,40, 47]//Array is an object, arr means where we can store multiple values in a single variable.
 const myHeroes = ["N","p","s"]
 
-const Arr2 = myArr
-// Arr2.shift(4)
+// console.log(myArr[0]);
+
+
+
+const Arr2 = myArr 
+
+
+
+// Arr2.shift()
 // console.log(Arr2);
 
 
 
 const myArr2 = new Array(1,2,3,4,5,6)
 
+
 // console.log(myArr2[3]);
 // console.log(myHeroes);
+
 // console.log(myArr2);
 
+
 //Array Methods
-// myArr2.push(6)
-// myArr2.push(7)
+
+
+// myArr2.push(6) 
+// myArr2.push(7) //add element
 // myArr2.pop()          //remove the last element
 // console.log(myArr2);
 
 
 // myArr.unshift(9)
-myArr.shift()
+
+// myArr.shift()
+
 // console.log(myArr.includes(9));
 // console.log(myArr);
 
 
+
 // console.log(typeof myArr);// if in [] then its obj but without [] its perform an string 
 
-// console.log(myArr.indexOf(9));
+// console.log(myArr.indexOf());
 // console.log(myArr.includes(9));
 
- const newArr = myArr .join()
+// const newArr = myArr .join()
+
 //  console.log(myArr);
 //  console.log(newArr);
  
 
 //  console.log(typeof newArr);
+
+
+
+
+
+
+
+
+
+
+
 
  // slice and spice
 
@@ -45,12 +72,17 @@ console.log("A ", myArr);
 const myn1 = myArr.slice(1, 3)
 
 console.log(myn1);
+
+
 console.log("B", myArr);
 const myn2 = myArr.splice(1, 3)//  diff between slice and splice In JavaScript, slice () and splice () are array methods with distinct purposes. `slice ()` creates a new array containing selected elements from the original, while `splice ()` modifies the original array by adding, removing, or replacing elements.
 
 
 console.log(myn2);
 console.log("C", myArr);
+
+
+
 
 
 
@@ -91,37 +123,37 @@ console.log("C", myArr);
 // // [3, 4, 5, 6, 7, 1, 2]
 
 
-class Solution {
-  nextPermutation(nums) {
-    let n = nums.length;
-    let i = n - 2;
+// class Solution {
+//   nextPermutation(nums) {
+//     let n = nums.length;
+//     let i = n - 2;
 
-    // Step 1: Find pivot
-    while (i >= 0 && nums[i] >= nums[i + 1]) {
-      i--;
-    }
+//     // Step 1: Find pivot
+//     while (i >= 0 && nums[i] >= nums[i + 1]) {
+//       i--;
+//     }
 
-    // Step 2: Find successor
-    if (i >= 0) {
-      let j = n - 1;
-      while (nums[j] <= nums[i]) {
-        j--;
-      }
-      // Step 3: Swap pivot and successor
-      [nums[i], nums[j]] = [nums[j], nums[i]];
-    }
+//     // Step 2: Find successor
+//     if (i >= 0) {
+//       let j = n - 1;
+//       while (nums[j] <= nums[i]) {
+//         j--;
+//       }
+//       // Step 3: Swap pivot and successor
+//       [nums[i], nums[j]] = [nums[j], nums[i]];
+//     }
 
-    // Step 4: Reverse the suffix
-    this.reverse(nums, i + 1, n - 1);
+//     // Step 4: Reverse the suffix
+//     this.reverse(nums, i + 1, n - 1);
 
-    return nums;
-  }
+//     return nums;
+//   }
 
-  reverse(arr, start, end) {
-    while (start < end) {
-      [arr[start], arr[end]] = [arr[end], arr[start]];
-      start++;
-      end--;
-    }
-  }
-}
+//   reverse(arr, start, end) {
+//     while (start < end) {
+//       [arr[start], arr[end]] = [arr[end], arr[start]];
+//       start++;
+//       end--;
+//     }
+//   }
+// }
