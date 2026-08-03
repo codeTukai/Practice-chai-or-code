15f8267aefe7d55f3a4eb17b20c826ed5fbd4739
codeTukai <tukaighosh753@gmail.com>
2026-08-03 14:09:15 +0530

    Add function to generate random token and log it to console

--- 01_basics/loop/three.js
+++ 01_basics/loop/three.js
@@ -55,3 +55,24 @@ const myObject ={
 
 //notes :- for of is used only map becoz it is not iterable with object i want to used loop on object then used for in 
         //    Map is not iterable its always return key value pairs 
+
+
+
+
+        function generateToken (){
+    let token = '';
+
+    const options = ['A', 'B', 'C', 'D', 'E', 'F','G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q','R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z','0', '1','2','3','4','5','6','7','8','9']
+
+    for (let i = 0; i < 36; i++) {
+        token += options[Math.floor(Math.random() * options.length)]
+
+       
+        
+    }
+     console.log(token);
+
+    return token
+}
+
+generateToken()
